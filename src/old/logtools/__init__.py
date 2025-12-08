9774d4c6620a9cd5061185eb8a27211a20b1b11c
Mark Zwart <mark.zwart@pobox.com>
2025-12-08 23:43:31 +0100

    Many fixes, deleted obsolete code

--- src/old/logtools/__init__.py
+++
@@ -1,4 +0,0 @@
-from .log_manager import get_logger, issue_tracker
-from .log_config import setup_logging
-
-__all__ = ["get_logger", "issue_tracker", "setup_logging"]
--- src/old/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .manager import manager
-from .editor import editor
-
-__all__ = ["manager", "editor"]
