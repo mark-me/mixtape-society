47b99af39c36f331184870ca2f5c24715a900895
Mark Zwart <mark.zwart@pobox.com>
2025-12-15 20:18:26 +0100

    Add save changes question when closing editor with unsaved changes.

--- src/app.py
+++ src/app.py
@@ -48,7 +48,7 @@ app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["500 per day", "100 per hour"],
 )
 app.secret_key = config.PASSWORD
 CORS(app)  # This adds Access-Control-Allow-Origin: * to ALL responses
